1d3417cccb89455101f8d4222714b99483f71b95
md-naim-molla <naim.molla.stats@gmail.com>
2026-06-09 20:42:16 +0600

    setup added

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
@@ -6,20 +6,19 @@ with open("README.md", "r", encoding="utf-8") as f:
 ## edit below variables as per your requirements -
 REPO_NAME = "End-to-End-Book-Recommender-System"
 AUTHOR_USER_NAME = "md-naim-molla"
-SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
 
 
 setup(
-    name=SRC_REPO,
+    name="book-recommender",
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for Movie Recommender System",
+    description="A small package for Book Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="naim.molla.stats@gmail.com",
-    packages=[SRC_REPO],
+    packages= find_packages(),
     license="MIT",
     python_requires=">=3.7",
     install_requires=LIST_OF_REQUIREMENTS
